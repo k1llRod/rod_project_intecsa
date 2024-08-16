6d4ba80cc213239f6209dfd55aa994223581e585
Rodrigo <kurt.gf@gmail.com>
2024-08-16 10:11:41 -0400

    [FIX]arreglar submodulo

---
+++ rod_sales/models/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from . import sale_order
+from . import sale_order_line
+from . import commission
+from . import res_partner
+from . import res_users
+from . import product_template
+from . import stock_move_line
+from . import stock_picking
