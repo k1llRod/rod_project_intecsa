af3eca0aa16569e3df179e5cc1e3ec27f9f3ee56
Rodrigo Loza <kurt.gf@gmail.com>
2025-04-21 01:39:25 -0400

    [FIX]arreglos

--- rod_sales/models/res_partner.py
+++ rod_sales/models/res_partner.py
@@ -7,4 +7,5 @@ class ResPartner(models.Model):
     date = fields.Date(string='Fecha de asignacion', default=fields.Date.today)
     commission_active = fields.Boolean(string='Activar comision', default=False)
 
-
+    hide_peppol_fields = fields.Boolean(string='Ocultar campos Peppol', default=False)
+    is_coa_installed = fields.Boolean(string='Chart of Accounts Installed', store=True)
