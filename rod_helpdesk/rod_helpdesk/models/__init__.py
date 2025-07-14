c8394db2602e80f14e12fd2cb02949aa180eb1d2
Rodrigo Loza <kurt.gf@gmail.com>
2025-07-13 22:48:51 -0400

    [FIX]arrelgos whatsapp

--- rod_helpdesk/rod_helpdesk/models/__init__.py
+++ rod_helpdesk/rod_helpdesk/models/__init__.py
@@ -2,3 +2,4 @@
 
 from . import helpdesk_accesory_line
 from . import helpdesk_ticket
+from . import sale_order
--- rod_helpdesk/rod_helpdesk/models/helpdesk_ticket.py
+++ rod_helpdesk/rod_helpdesk/models/helpdesk_ticket.py
@@ -1,5 +1,6 @@
 # models/helpdesk_ticket.py
 from odoo import models, fields
+from odoo.exceptions import UserError
 
 class HelpdeskTicket(models.Model):
     _inherit = 'helpdesk.ticket'
@@ -8,3 +9,23 @@ class HelpdeskTicket(models.Model):
     marca = fields.Char(string='Marca', help="Marca del producto o accesorio entregado")
     modelo = fields.Char(string='Modelo', help="Modelo del producto o accesorio entregado")
     serial_number = fields.Char(string='Numero serie', help="Número de serie del producto o accesorio entregado")
+    sale_order_ids = fields.One2many('sale.order', 'ticket_id', string='Ventas')
+    def action_create_sale_order(self):
+        for ticket in self:
+            if not ticket.partner_id:
+                raise UserError("Debe asignar un cliente al ticket antes de crear una venta.")
+
+            sale = self.env['sale.order'].create({
+                'partner_id': ticket.partner_id.id,
+                'origin': ticket.name,
+                'client_order_ref': ticket.description or ticket.name,
+                'ticket_id': ticket.id if 'ticket_id' in self.env['sale.order']._fields else False,
+            })
+            ticket.sale_order_id = sale.id
+            return {
+                'type': 'ir.actions.act_window',
+                'res_model': 'sale.order',
+                'res_id': sale.id,
+                'view_mode': 'form',
+                'target': 'current',
+            }
