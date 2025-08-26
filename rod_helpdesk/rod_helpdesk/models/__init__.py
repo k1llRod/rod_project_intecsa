ce906bd76df692045839697030dddbbcb270a1d8
Rodrigo Loza <kurt.gf@gmail.com>
2025-08-25 23:17:50 -0400

    [DEL]

--- rod_helpdesk/rod_helpdesk/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import sale_order
-from . import helpdesk_ticket
-from . import helpdesk_accesory_line
-
-
--- rod_helpdesk/rod_helpdesk/models/sale_order.py
+++
@@ -1,7 +0,0 @@
-
-from odoo import models, fields
-
-class SaleOrder(models.Model):
-    _inherit = 'sale.order'
-
-    ticket_id = fields.Many2one('helpdesk.ticket', string='Ticket de Soporte')
